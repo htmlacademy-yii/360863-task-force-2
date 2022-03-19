e911b44ad823d1dcdb80bf9dcddbeb5d8e6f5c85
Leonid <alraun_work@mail.ru>
2022-03-19 23:32:36 +0300

    1. Вроде сделал задание, но какая то странная ошибка в созданном файле city.sql почему то пишет что нет поля title в таблице, хотя поле по факту есть. 2. Плюс в слзданных файлах какие-то проблемы с переносом на новые строки, почему то не работает ->next(); Если я правильно понял, что этот метод должен переносить на следующую строку

---
+++ data/category.sql
@@ -0,0 +1,2 @@
+use task_force;insert into category ( title,icon
+ ) values('Курьерские услуги','translation'),('Уборка','clean'),('Переезды','cargo'),('Компьютерная помощь','neo'),('Ремонт квартирный','flat'),('Ремонт техники','repair'),('Красота','beauty'),('Фото','photo')
