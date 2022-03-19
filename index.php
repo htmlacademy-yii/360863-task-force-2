048134d4f386cece2006ff4ceaa665a7ba806675
Leonid <alraun_work@mail.ru>
2022-03-19 23:32:22 +0300

    1. Вроде сделал задание, но какая то странная ошибка в созданном файле city.sql почему то пишет что нет поля title в таблице, хотя поле по факту есть. 2. Плюс в слзданных файлах какие-то проблемы с переносом на новые строки, почему то не работает ->next(); Если я правильно понял, что этот метод должен переносить на следующую строку

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use TaskForce\RejectAction;
 use TaskForce\TakeAction;
 use TaskForce\ActionException;
 use TaskForce\StatusException;
+use TaskForce\ConverterCsvSql;
 
 require_once 'vendor/autoload.php';
 
@@ -15,9 +16,21 @@ print_r($task->getNextStatus('cancel')); print_r('<br>');
 print_r($task->getActions( 'new')); print_r('<br>');*/
 
 
-try {
+/*try {
     $task = new TaskForce\TaskStrategy(1, 2, 2);
     print_r($task->getActions( 'newx')); print_r('<br>');
 } catch (Exception $e) {
     die($e->getMessage());
-};
+};*/
+
+$category = new \TaskForce\ConverterCsvSql('data/category.csv');
+$category->import();
+
+$city = new ConverterCsvSql('data/city.csv');
+$city->import();
+
+
+
+/*if(is_numeric(55.89976)){
+    print_r(1);
+};*/
---
+++ src/ConverterCsvSql.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace TaskForce;
+
+class ConverterCsvSql
+{
+    public  string $csvFilename;
+    const DATABASE_NAME = 'task_force';
+    public  string $tableName;
+    public  object $csvFileObject;
+    public  object $sqlFileObject;
+    public string $columns;
+
+    public function __construct(string $filename)
+    {
+        $this->csvFilename = $filename;
+    }
+
+    public function import():void
+    {
+        $name = explode('.', $this->csvFilename);
+        $filename = explode('/', $name[0]);
+
+        $this->sqlFilename = $name[0] . '.sql';
+        $this->tableName = array_pop($filename);
+
+        $this->csvFileObject = new \SplFileObject($this->csvFilename);
+        $this->csvFileObject->rewind();
+        $this->columns = $this->csvFileObject->current();
+        $this->csvFileObject->next();
+
+        $this->sqlFileObject = new \SplFileObject($this->sqlFilename, 'w');
+        $this->sqlFileObject->rewind();
+        $this->sqlFileObject->fwrite( 'use' . ' ' . self::DATABASE_NAME . ';');
+        $this->sqlFileObject->next();
+        $this->sqlFileObject->fwrite("insert into $this->tableName ( $this->columns ) values");
+
+        $array = [];
+        foreach ($this->getNextLine() as $value) {
+            $array[] = "($value)";
+        }
+        $array = implode(',' , $array);
+        $this->sqlFileObject->fwrite($array);
+
+    }
+
+    private function getNextLine():?iterable {
+
+        while (!$this->csvFileObject->eof()) {
+            $array =[];
+            $result = $this->csvFileObject->fgetcsv();
+
+            foreach($result as $value) {
+                if(!is_numeric($value)) {
+                    $array[] = "'$value'";
+                } else {
+                    $array[] = "$value";
+                }
+            }
+            yield implode(',', $array);
+        }
+    }
+}
