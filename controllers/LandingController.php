16a2ce9aac791dcd1b8cc315647f9d593c8576e7
Leonid <alraun_work@mail.ru>
2022-06-09 21:10:20 +0300

    git test

--- controllers/LandingController.php
+++ controllers/LandingController.php
@@ -27,6 +27,7 @@ class LandingController extends AppController
             if ($authorization->load(\Yii::$app->request->post()) && $authorization->validate()) {
                 var_dump(11111111111111111111111111);
             }
+
         }
 
 
